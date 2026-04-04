d13077db548ebbbaee50b678787f0b9c503cae95
unknown <phfdea13@gmail.com>
2026-04-04 16:16:01 -0300

    first commit

---
+++ src/main/java/dev/java10x/Cadastronew/CadastronewApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.Cadastronew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastronewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastronewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/Cadastronew/CadastronewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.Cadastronew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastronewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
